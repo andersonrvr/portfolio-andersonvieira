255dd053bfc8be36a2618e95023113281c4c7ee4
Anderson Vieira <andersonrvr@gmail.com>
2022-08-05 10:25:20 -0300

    update: career objective

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -9,31 +9,19 @@ const Home = () => {
       <Header />
       <div className="mainHome">
         <div className="mySelf">
-          <TypeAnimation
-            cursor={false}
-            sequence={["Anderson", 1000, "Anderson Vieira"]}
-            wrapper="h1"
-          />
+          <TypeAnimation cursor={false} sequence={["Anderson", 1000, "Anderson Vieira"]} wrapper="h1" />
 
-          <h2>Desenvolvedor Front-End</h2>
+          <h2>Desenvolvedor Full Stack</h2>
         </div>
         <div className="aside">
           <Technologies />
         </div>
       </div>
       <div className="contacts">
-        <a
-          href="https://github.com/andersonrvr"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://github.com/andersonrvr" target="_blank" rel="noreferrer">
           Github
         </a>
-        <a
-          href="https://www.linkedin.com/in/andersonvieira-dev/"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://www.linkedin.com/in/andersonvieira-dev/" target="_blank" rel="noreferrer">
           Linkedin
         </a>
       </div>
